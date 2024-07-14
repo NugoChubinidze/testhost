17272d4acb30eb46b872ddf8498a7ca0757d3cc8
tazo <barbareiaganashvili123@gmail.com>
2024-07-14 15:08:53 +0400

    lasttry

--- public/js/wineData.js
+++ public/js/wineData.js
@@ -1,6 +1,5 @@
 async function displayWineDetails(hash) {
     const detailsContainer = document.getElementById('wine-details');
-    const wineContainer = document.getElementsByClassName('wine');
     const data = document.querySelectorAll('.data');
     if (!detailsContainer) {
         console.error('Details container not found!');
@@ -21,21 +20,7 @@ async function displayWineDetails(hash) {
             }
         });
 
-        // detailsContainer.innerHTML = `
-        //     <p>Name: ${wineDetails[0]}</p>
-        //     <p>Date: ${wineDetails[1]}</p>
-        //     <p>Manufacturer: ${wineDetails[2]}</p>
-        //     <p>Type: ${wineDetails[3]}</p>
-        // `;
 
-        // wineContainer.innerHTML = `
-        //     <div class="brandName">${wineDetails[2]}</div>
-        //     <div class="details">
-        //         <div class="age">${wineDetails[1]}</div>
-        //         <div class="species">${wineDetails[3]}</div>
-        //     </div>
-        //     <div class="description">${wineDetails[0]}</div>
-        // `
     } catch (error) {
         console.error('Error fetching wine details:', error);
     }
